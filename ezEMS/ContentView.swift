a0ab3a104cc95e4f8f9463b57b6c0eecf1609690
yoohyukchang <yoohyukchang@gmail.com>
2023-06-15 17:27:45 +0900

    added trained data file. updated the code to use SwiftyTesseract to identify texts inside the image.

--- ezEMS/ContentView.swift
+++ ezEMS/ContentView.swift
@@ -1,21 +1,59 @@
 //
 //  ContentView.swift
-//  ezEMS
+//  ezEMSApp
 //
 //  Created by Yoohyuk Chang on 6/15/23.
 //
 
 import SwiftUI
+import SwiftyTesseract
+import Combine
+
+class ContentViewModel: ObservableObject {
+    @Published var recognizedText: String = ""
+
+    func recognizeText() {
+        let tesseract = Tesseract(languages: [.english])
+        guard let image = UIImage(named: "test_image") else {
+            print("Image not found!")
+            return
+        }
+
+        print(image)
+
+        let publisher: AnyPublisher<String, Tesseract.Error> = tesseract.performOCRPublisher(on: image)
+
+        publisher
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("Error: \(error)")
+                    case .finished:
+                        print("Finished recognizing text.")
+                    }
+                },
+                receiveValue: { recognizedText in
+                    self.recognizedText = recognizedText
+                }
+            )
+            .store(in: &cancellables)
+    }
+    
+    private var cancellables: Set<AnyCancellable> = []
+}
 
 struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("This is for testing")
+            Text(viewModel.recognizedText)
+            Button(action: viewModel.recognizeText) {
+                Text("Test")
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +62,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
