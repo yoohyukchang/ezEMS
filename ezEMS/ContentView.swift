fbfe75020d3b27bedee56d70066f993dc3967296
yoohyukchang <yoohyukchang@gmail.com>
2023-06-15 17:13:32 +0900

    Initial Commit

---
+++ ezEMS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ezEMS
+//
+//  Created by 장유혁 on 6/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ezEMS/ezEMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ezEMSApp.swift
+//  ezEMS
+//
+//  Created by 장유혁 on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ezEMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
