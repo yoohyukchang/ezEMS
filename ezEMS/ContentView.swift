df8127a59a8d96d8c9d7f79a8294370bd25aac4c
yoohyukchang <yoohyukchang@gmail.com>
2023-06-15 20:24:14 +0900

    separated the contentview and contentviewmodel

--- ezEMS/ContentView.swift
+++ ezEMS/ContentView.swift
@@ -6,43 +6,6 @@
 //
 
 import SwiftUI
-import SwiftyTesseract
-import Combine
-
-class ContentViewModel: ObservableObject {
-    @Published var recognizedText: String = ""
-
-    func recognizeText() {
-        let tesseract = Tesseract(languages: [.english])
-        guard let image = UIImage(named: "test_image") else {
-            print("Image not found!")
-            return
-        }
-
-        print(image)
-
-        let publisher: AnyPublisher<String, Tesseract.Error> = tesseract.performOCRPublisher(on: image)
-
-        publisher
-            .receive(on: DispatchQueue.main)
-            .sink(
-                receiveCompletion: { completion in
-                    switch completion {
-                    case .failure(let error):
-                        print("Error: \(error)")
-                    case .finished:
-                        print("Finished recognizing text.")
-                    }
-                },
-                receiveValue: { recognizedText in
-                    self.recognizedText = recognizedText
-                }
-            )
-            .store(in: &cancellables)
-    }
-    
-    private var cancellables: Set<AnyCancellable> = []
-}
 
 struct ContentView: View {
     @StateObject var viewModel = ContentViewModel()
---
+++ ezEMS/ContentViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  ContentViewModel.swift
+//  ezEMS
+//
+//  Created by Yoohyuk Chang on 6/15/23.
+//
+
+import SwiftUI
+import SwiftyTesseract
+import Combine
+
+class ContentViewModel: ObservableObject {
+    @Published var recognizedText: String = ""
+
+    func recognizeText() {
+        let tesseract = Tesseract(languages: [.english])
+        guard let image = UIImage(named: "test_image2") else {
+            print("Image not found!")
+            return
+        }
+
+        print(image)
+
+        let publisher: AnyPublisher<String, Tesseract.Error> = tesseract.performOCRPublisher(on: image)
+
+        publisher
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("Error: \(error)")
+                    case .finished:
+                        print("Finished recognizing text.")
+                    }
+                },
+                receiveValue: { recognizedText in
+                    self.recognizedText = recognizedText
+                }
+            )
+            .store(in: &cancellables)
+    }
+    
+    private var cancellables: Set<AnyCancellable> = []
+}
